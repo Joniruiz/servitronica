fd8118e1f51d4202b4a03da7c2b902f795779b17
KevinAlmada <kevinalmada81@gmail.com>
2021-10-27 16:56:38 -0300

    menu burger funcional

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,11 @@
 window.addEventListener("load",() => {
     let hamburger = document.querySelector(".hamburger");
+    let menuMobile = document.querySelector(".menuMobile")
      
     hamburger.addEventListener("click", function() {
         hamburger.classList.toggle("is-active");
+        menuMobile.classList.toggle("hidden");
+        menuMobile.classList.toggle("deploy");
     });
    
 })
