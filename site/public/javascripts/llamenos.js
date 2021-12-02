1e298886bb6e4599aa12c3704ddd7647677b0e05
KevinAlmada <kevinalmada81@gmail.com>
2021-12-01 23:19:01 -0300

    brands and js fix

--- site/public/javascripts/llamenos.js
+++ site/public/javascripts/llamenos.js
@@ -1,12 +1,20 @@
+let _llamemos = document.querySelector(".llamenos")
+let _menuMobile = document.querySelector(".menuMobile")
+let _hamburger = document.querySelector(".hamburger")
+
+
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY >= 2600){
-       document.querySelector(".llamenos").style.display= "none"
+    if(window.scrollY >= 2700){
+        _llamemos.style.display= "none"
+       if (_menuMobile.classList.contains("deploy")) {
+        _menuMobile.classList.remove("deploy")
+        _menuMobile.classList.add("hidden")
+        _hamburger.classList.remove("is-active")
+       }
       
    }else {
-       document.querySelector(".llamenos").style.display="flex"
-      
-     
+        _llamemos.style.display="flex"
    }
    
   })
